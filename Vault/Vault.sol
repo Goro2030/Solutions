a785c2916cd6624802847bbfc9d322c34107a737
cryptowea <37342734+cryptowea@users.noreply.github.com>
2018-05-14 13:16:41 -0400

    Update Vault.sol
    fixd

--- Vault/Vault.sol
+++ Vault/Vault.sol
@@ -1,10 +1,26 @@
 pragma solidity 0.4.18;
 
+/**
+ * @title Vault WEA 
+ * @author  WEA CEO
+ * @version 0.1 Beta
+ *
+ * @section LICENSE
+ * Contrato con registros en DIBAM, Santiago de Chile,
+ * cualquier uso, copia u raminifación de este código está
+ * extrictamente prohibido, así como también el código del
+ * frontend que resulte de mencionado contrato.
+ *
+ */
+
 contract Vault {
 
     address public  owner;
     uint public time;
 
+    /**
+    *@dev change those names for log
+    **/
     event retire (string mensaje, uint monto);
     event deposit (string mensaje, uint monto);
 
@@ -12,7 +28,10 @@ contract Vault {
         owner = msg.sender;
         time = _time;
     }
-
+    
+    /**
+    *@dev no modifier required
+    **/
     function reclaim() public {
         require(msg.sender == owner && time < now);
         owner.transfer(this.balance);
